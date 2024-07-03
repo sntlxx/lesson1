866125572c4874210e1da2b29b6994edd78f8b2b
Natalie <sonatalex@gmail.com>
2024-07-03 23:18:46 +0300

    add lesson1task4

---
+++ Desktop/LESSON1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = -2;
+int b = -1;
+int c = -8;
+int d = -9;
+int e = -6;
+
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine (max);
