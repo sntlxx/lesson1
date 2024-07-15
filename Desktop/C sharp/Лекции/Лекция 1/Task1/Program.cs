90f9e7583d156224ff14e9426e05c41d2b742106
Natalie <sonatalex@gmail.com>
2024-07-16 00:51:38 +0300

    done

--- Desktop/C sharp/Лекции/Лекция 1/Task1/Program.cs
+++ Desktop/C sharp/Лекции/Лекция 1/Task1/Program.cs
@@ -1,2 +1,7 @@
 ﻿
-Console.WriteLine("Hello, World!");
+// int value = 100;
+// System.Console.WriteLine(value);
+// double number = 10.5;
+// System.Console.WriteLine(number);
+char c = '!';
+System.Console.WriteLine(c);
