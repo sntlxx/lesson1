13602a5b867ea23989bd11fdf5441126c6f1beb2
Natalie <sonatalex@gmail.com>
2024-07-19 00:59:06 +0300

    task1 is done

---
+++ Desktop/C sharp/Практика/Семинары/семинар 4/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿void Main()
+{
+while(true)
+{
+    Console.Write("Введите число или нажмите 'q', чтобы завершить программу: ");
+    string input = Console.ReadLine(); 
+ 
+ if (input == "q") 
+ {
+ break; 
+ }
+
+ bool isNumber = int.TryParse(input, out int number); 
+ 
+ if (!isNumber) 
+ {
+ Console.WriteLine("Вы ввели не число. Попробуйте снова.");
+ continue; 
+ }
+ 
+ int sum = 0; 
+ 
+ while (number > 0) 
+ {
+ sum += number % 10; 
+ number /= 10; 
+ }
+
+ if (sum % 2 == 0) 
+ {
+ Console.WriteLine("Сумма цифр четная. Программа завершена.");
+ break; 
+ }
+ }
+ }
+
+Main();
