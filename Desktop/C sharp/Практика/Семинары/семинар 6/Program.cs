cbd9af2d2f33c46b72662d502676bf0d4d39dd19
Natalie <sonatalex@gmail.com>
2024-07-29 00:00:01 +0300

    задача 1 решена

---
+++ Desktop/C sharp/Практика/Семинары/семинар 6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива. 
+// a b c => “abcdef”
+// d e f 
+
+void Main ()
+{
+char[,] matrix = 
+{
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f' }
+   
+};
+string str = "";
+foreach(char ch in matrix)
+   { 
+    str += ch;
+   }
+System.Console.WriteLine(str);
+}
+Main();
