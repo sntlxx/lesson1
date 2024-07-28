d2642f6b29d23c51f73ac3408ad2d9a778ad38f5
Natalie <sonatalex@gmail.com>
2024-07-29 00:41:41 +0300

    Task3 done

---
+++ Desktop/C sharp/Практика/ДЗ к семинарам/ДЗ семинар 6/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+void Main()
+{
+System.Console.WriteLine("Введите строку: ");
+string? str = Console.ReadLine(); 
+
+string? reverse =  new string(str!.ToCharArray().Reverse().ToArray());
+
+if(str.Equals(reverse))
+{
+   System.Console.WriteLine("Палиндром");
+}
+else
+{
+    System.Console.WriteLine("Не палиндром");
+}
+}
+Main();
