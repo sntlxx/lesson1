1c18ba9347e2c7901ab521947b9fd30668779ed0
Natalie <sonatalex@gmail.com>
2024-07-31 00:13:34 +0300

    Task1 is done

---
+++ Desktop/C sharp/Практика/ДЗ к семинарам/ДЗ семинар 7/Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы
+
+void Main()
+ {
+ Console.WriteLine("Введите начальное значение M:");
+ int M = Convert.ToInt32(Console.ReadLine());
+ 
+ Console.WriteLine("Введите конечное значение N:");
+ int N = Convert.ToInt32(Console.ReadLine());
+
+ if(M > N)
+ {
+ Console.WriteLine("Начальное значение не может быть больше конечного. Введите числа заново.");
+ return;
+ }
+ 
+ PrintNumbers(M, N);
+ }
+
+void PrintNumbers(int first, int second)
+ {
+ if (first > second) 
+{
+    return;
+}
+ Console.Write(first+ " ");
+ PrintNumbers(first+ 1, second);
+ }
+
+
+
+Main();
